256249cabab323149e70d4bb650a2db0921cf31d
fengbin_chen <1563540102@qq.com>
2025-12-24 09:56:49 +0800

    add PageWrapper component !!!

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Question.module.scss";
+import PageWrapper from "@/components/PageWrapper";
 import QuestionInput from "@/components/QuestionComponents/QuestionInput";
 import QuestionRadio from "@/components/QuestionComponents/QuestionRadio";
 
@@ -12,41 +13,43 @@ export default function Question(props: PropsType) {
 
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Question</title>
         <meta name="description" content="Question page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <form method="post" action="/api/answer">
-        {/* 隐藏域提交id */}
-        <input type="hidden" name="questionId" value={id} />
-        <div className={styles.componentWrapper}>
-          <QuestionInput
-            fe_id={"123"}
-            props={{ title: "test", placeholder: "test" }}
-          />
-        </div>
-        <div className={styles.componentWrapper}>
-          <QuestionRadio
-            fe_id={"1234"}
-            props={{
-              title: "test",
-              options: [
-                { label: "test1", value: "test1" },
-                { label: "test2", value: "test21" },
-                { label: "test3", value: "test3" },
-              ],
-              value: "test",
-              vertical: true,
-            }}
-          />
-        </div>
-        <div className={styles.submitBtnContainer}>
-          {/* <input type="submit" value="提交" /> */}
-          <button type="submit">提交</button>
-        </div>
-      </form>
+      </Head> */}
+      <PageWrapper title="Question" desc="Question page">
+        <form method="post" action="/api/answer">
+          {/* 隐藏域提交id */}
+          <input type="hidden" name="questionId" value={id} />
+          <div className={styles.componentWrapper}>
+            <QuestionInput
+              fe_id={"123"}
+              props={{ title: "test", placeholder: "test" }}
+            />
+          </div>
+          <div className={styles.componentWrapper}>
+            <QuestionRadio
+              fe_id={"1234"}
+              props={{
+                title: "test",
+                options: [
+                  { label: "test1", value: "test1" },
+                  { label: "test2", value: "test21" },
+                  { label: "test3", value: "test3" },
+                ],
+                value: "test",
+                vertical: true,
+              }}
+            />
+          </div>
+          <div className={styles.submitBtnContainer}>
+            {/* <input type="submit" value="提交" /> */}
+            <button type="submit">提交</button>
+          </div>
+        </form>
+      </PageWrapper>
     </>
   );
 }
